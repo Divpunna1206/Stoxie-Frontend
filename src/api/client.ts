b3b8e691fccecfc19bd8a88a11482df9dcda633f
Divya Punna <divyapunna1206@gmail.com>
2025-12-12 13:02:44 +0530

    new api added

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   // import.meta.env.VITE_API_URL ?? "http://localhost:8000";
-  import.meta.env.VITE_API_URL ?? "http://103.148.165.178:8000";
+  import.meta.env.VITE_API_URL ?? "http://103.178.113.168:8000";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
